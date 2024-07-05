dce03f24047a92ea7ab9169143d15cff2b339622
Otoniel De Oliveira | A2IT PT-LIS <ooliveira@pt.a2it.com>
2024-07-05 15:58:29 +0100

    Versao Inicial

---
+++ script.js
@@ -0,0 +1,25 @@
+const dia = document.getElementById("dia")
+const hora = document.getElementById("hora")
+const minuto = document.getElementById("minuto")
+const segundo = document.getElementById("segundo")
+
+const lancamento = "1 dec 2024"
+
+function countDown(){
+    const dataLanc = new Date(lancamento);
+    const hoje = new Date();
+
+    const segTotal = (dataLanc - hoje)/1000;
+    const finalDias = Math.floor(segTotal / 60 / 60 / 24);
+    const finalHoras = Math.floor(segTotal / 60 / 60) % 24;
+    const finalMinutos = Math.floor(segTotal / 60) % 60;
+    const finalSegundos = Math.floor(segTotal) % 60;
+    
+    dia.innerHTML = `<strong>${finalDias}D</strong>`
+    hora.innerHTML = `<strong>${finalHoras}H</strong>`
+    minuto.innerHTML = `<strong>${finalMinutos}M</strong>`
+    segundo.innerHTML = `<strong>${finalSegundos}S</strong>`
+}
+
+countDown();
+setInterval(countDown, 1000)
